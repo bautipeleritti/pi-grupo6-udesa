b8eef87607e50a96c22d13d6c50439b0f9fca546
ignaciobianco <ibianco@udesa.edu.ar>
2023-11-14 14:46:26 -0300

    detalles genero

--- js/detallegenero.js
+++ js/detallegenero.js
@@ -1,13 +1,18 @@
 let acaVaLaAPIKey = "e085a8d4a0502afc1d3c8e65c53af130";
 let urlparams = new URLSearchParams(location.search);
 let qsIdGenero = urlparams.get('id');
-const discoverEndpoint = "https://api.themoviedb.org/3/discover/movie";
 
+// Endpoints 
+const discoverEndpoint = "https://api.themoviedb.org/3/discover/movie";
+const discoverEndpointSeries = 'https://api.themoviedb.org/3/discover/tv';
+
+
+// IDs
 qsIddiv = document.querySelector('#detallesgeneros_id')
 
 // Construye la URL de la solicitud para obtener películas recomendadas para el género seleccionado
 const urlPeliculasRecomendadas = `${discoverEndpoint}?api_key=${acaVaLaAPIKey}&with_genres=${qsIdGenero}`;
-
+const urlSeriesRecomendadas = `${discoverEndpointSeries}?api_key=${acaVaLaAPIKey}&with_genres=${qsIdGenero}`;
 
 
 
@@ -52,3 +57,57 @@ qsIddiv.innerHTML = contenido;
     .catch(function (error) {
         console.log(error);
     });
+
+
+
+// Realiza la solicitud para obtener series recomendadas
+
+
+fetch(urlSeriesRecomendadas)
+
+
+
+.then(function (res) {
+    return res.json();
+
+
+
+})
+
+
+
+
+.then(function (data) {
+    console.log(data);
+    miData = data.results;
+    contenido = ' '
+
+    for (let i = 0; i < 5; i++) {
+
+    contenido +=   
+    `<section class="sectionpadrepelis">
+    <div class="contenedor">
+        <h2 class="titulopadre"></h2>
+<div class="caja"><a href="./detallepelicula.html?id=${MiData[i].id}"><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}" class="pulp">
+     <h3 class="titulopeli">${miData[i].title}</h3>
+     
+     </div>
+
+
+     </section>
+     `
+
+
+
+
+    }
+
+
+
+qsIddiv.innerHTML = contenido;
+
+
+})
+.catch(function (error) {
+    console.log(error);
+});
--- js/detallepelicula.js
+++ js/detallepelicula.js
@@ -4,7 +4,8 @@ let urlparams = new URLSearchParams(location.search);
 
 let qsIdPelicula = urlparams.get('id');
 
-
+const urlPeliculasRecomendadas = `${discoverEndpoint}?api_key=${acaVaLaAPIKey}&with_genres=${qsIdGenero}`;
+const urlSeriesRecomendadas = `${discoverEndpointSeries}?api_key=${acaVaLaAPIKey}&with_genres=${qsIdGenero}`;
 
 let DetallePelicula = `https://api.themoviedb.org/3/movie/${qsIdPelicula}?api_key=${acaVaLaAPIKey}`
 
